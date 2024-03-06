dc01ef6010f15af3ee23a70e94308614c0281498
egororo <egorsemirenko04@gmail.com>
2024-03-07 00:08:33 +0300

    first steps

--- ray_casting/main.cpp
+++ ray_casting/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+
 int main(){
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Ray casting", sf::Style::Titlebar | sf::Style::Close);
     window.setFramerateLimit(60);
-    getchar();
+
+    sf::Shader shader;
+	shader.loadFromFile("shader.frag", sf::Shader::Fragment);
+    
+    while(window.isOpen()){
+        sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+        }
+
+        window.display();
+    }
+
     return 0;
 }
